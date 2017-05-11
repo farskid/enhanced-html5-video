9d20b2e2d4aefd36ee1769997e9735137279a89a
Farzad YZ <farskid@gmail.com>
2017-05-11 20:20:40 +0430

    moved cover image to another branch

--- video.js
+++ video.js
@@ -8,51 +8,10 @@ var enhancedHTML5Video = (function () {
     }
   }
 
-  function imageFactory(src, attrs) {
-    var img = new Image()
-
-    for (var prop in attrs) {
-      img[prop] = attrs[prop]
-    }
-
-    img.src = src
-
-    return img
-  }
-
-  function setElementsStyles(elements) {
-    elements.container.style.position = 'relative'
-
-    elements.cover.style.position = 'absolute'
-    elements.cover.style.top = '0px'
-    elements.cover.style.left = '0px'
-  }
-
   return {
     init: function (config) {
 
       if (!config.id) throw Error('enhancedHTML5Video requires a valid id for the video')
-
-      if (config.cover) {
-        var elements = queryElements(config.id)
-        var video = elements.video,
-          container = elements.container,
-          img
-
-        video.addEventListener('loadedmetadata', function () {
-          img = imageFactory(video.getAttribute('data-cover'), {
-            width: video.videoWidth,
-            height: video.videoHeight
-          })
-
-          container.appendChild(img)
-          setElementsStyles({
-            container: container,
-            cover: img
-          })
-        })
-
-      }
 
       document.addEventListener('keydown', function (e) {
         switch (e.which) {
