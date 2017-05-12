8123e1e13b315f00081f49508abc8ff7ff908aee
Farzad YZ <farskid@gmail.com>
2017-05-12 16:43:51 +0430

    added enter, doubliclick and singleclick bindings

--- video.js
+++ video.js
@@ -8,17 +8,64 @@ window.enhancedHTML5Video = (function () {
     }
   }
 
+  function togglePlayingState(video) {
+    if (video.paused) video.play()
+    else video.pause()
+  }
+
+  function toggleFullScreen(video) {
+    if (
+      !document.fullscreenElement &&
+      !document.mozFullScreenElement &&
+      !document.webkitFullscreenElement &&
+      !document.msFullscreenElement
+    ) {
+      // Video should enter fullscreen
+      if (video.requestFullscreen) {
+        video.requestFullscreen();
+      } else if (video.msRequestFullscreen) {
+        video.msRequestFullscreen();
+      } else if (video.mozRequestFullScreen) {
+        video.mozRequestFullScreen();
+      } else if (video.webkitRequestFullscreen) {
+        video.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      }
+    }
+    else {
+      // Video should exit fullscreen
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      }
+    }
+  }
+
   return {
     init: function (config) {
 
       if (!config.id) throw Error('enhancedHTML5Video requires a valid id for the video')
 
+      var videoElem = queryElements(config.id).video
+
+      videoElem.addEventListener('dblclick', function (e) {
+        toggleFullScreen(videoElem)
+      })
+
+      videoElem.addEventListener('click', function (e) {
+        togglePlayingState(videoElem)
+      })
+
       document.addEventListener('keydown', function (e) {
         switch (e.which) {
           case 32:
-            var videoElem = queryElements(config.id).video
-            if (videoElem.paused) videoElem.play()
-            else videoElem.pause()
+            togglePlayingState(videoElem)
+          case 13:
+            toggleFullScreen(videoElem)
         }
       })
     }
