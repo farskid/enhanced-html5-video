cb60a3079c4848cdde5cf64aee7083a1d81c9137
Farzad YZ <farskid@gmail.com>
2017-05-12 17:42:56 +0430

    config of key bindings

--- video.js
+++ video.js
@@ -1,5 +1,24 @@
 window.enhancedHTML5Video = (function () {
 
+  // Immutable Extend
+  function _extend(source, target) {
+
+    // Pass source to result by value
+    var result = JSON.parse(JSON.stringify(source))
+
+    // Return the source object when there is no `target` provided
+    if (!target) return source
+
+    // Extend logic: by here, the result has at least source's keys and values
+    for (var key in source) {
+      if (source.hasOwnProperty(key)) {
+        result[key] = !target.hasOwnProperty(key) ? source[key] : target[key]
+      }
+    }
+
+    return result
+  }
+
   function queryElements(id) {
     var container = document.getElementById(id)
     return {
@@ -50,22 +69,38 @@ window.enhancedHTML5Video = (function () {
 
       if (!config.id) throw Error('enhancedHTML5Video requires a valid id for the video')
 
+      // Extend default key bindings with `config.keys`
+      var keys = _extend({
+        doubleClick: true,
+        click: true,
+        enter: true,
+        spacebar: true
+      }, config.keys)
+
+      // Video Element
       var videoElem = queryElements(config.id).video
 
-      videoElem.addEventListener('dblclick', function (e) {
+      // Control Bindings
+      // Double Click: Toggle Fullscreen
+      keys.doubleClick && videoElem.addEventListener('dblclick', function (e) {
         toggleFullScreen(videoElem)
       })
-
-      videoElem.addEventListener('click', function (e) {
+      // Single Click: Toggle Playing State
+      keys.click && videoElem.addEventListener('click', function (e) {
         togglePlayingState(videoElem)
       })
 
+      // Keys
       document.addEventListener('keydown', function (e) {
         switch (e.which) {
+          // Spacebar: Toggle Playing State
           case 32:
-            togglePlayingState(videoElem)
+            keys.spacebar && togglePlayingState(videoElem)
+            break
+          // Enter: Toggle Fullscreen
           case 13:
-            toggleFullScreen(videoElem)
+            keys.enter && toggleFullScreen(videoElem)
+            break
         }
       })
     }
